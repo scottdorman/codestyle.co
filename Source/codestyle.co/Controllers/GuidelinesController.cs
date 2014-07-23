57324a62b15e130e4a2ae1bb8e0a998771d79891
Scott Dorman <scottdorman@outlook.com>
2014-07-23 23:13:41 +0000

    Initial checkin.
    git-tfs-id: [https://scottdorman.visualstudio.com]$/codestyle.co;C121

---
+++ Source/codestyle.co/Controllers/GuidelinesController.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+
+namespace codestyle.co.Controllers
+{
+    public class GuidelinesController : Controller
+    {
+        // GET: Guidelines
+        public ActionResult Index(string id)
+        {
+            ViewBag.Language = id;
+            var model = ControllerUtilities.GetContents(id);
+            if (model != null)
+            {
+                ViewBag.Title = "Guidelines for " + (string)model["title"];
+                return View(model);
+            }
+
+            ViewBag.Title = "Not found";
+            return View("NotFound");
+        }
+    }
+}
---
+++ Source/codestyle.co/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace codestyle.co.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home";
+            var model = ControllerUtilities.GetTableOfContents();
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Title = "About";
+            return View();
+        }
+    }
+}
