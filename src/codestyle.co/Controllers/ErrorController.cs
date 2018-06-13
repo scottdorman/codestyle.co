05987d5494172bd0a64ba3ae5a9271fc40dbd4a0
Scott Dorman <scottdorman@outlook.com>
2018-06-13 11:38:48 -0400

    Renamed Source folder.

---
+++ src/codestyle.co/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace codestyle.co.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 404;
+            Response.TrySkipIisCustomErrors = true;
+            return View();
+        }
+    }
+}
